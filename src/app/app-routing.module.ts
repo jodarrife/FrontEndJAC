96f9327465d907ab0c77cafe2aff239cdace5b3e
jodarrife <jodarrife12@gmail.com>
2022-03-13 00:31:12 -0500

    Organizar Carpetas Proyecto
    Se organizo las carpetas y todo los componentes iniciales del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/inicio/login/login.component';
+import { RegisterComponent } from './components/inicio/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  {
+    path: 'inicio',
+    component: InicioComponent,
+    children: [
+      { path: '', component: BienvenidaComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent }
+    ],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { LoginComponent } from './components/inicio/login/login.component';
+import { RegisterComponent } from './components/inicio/register/register.component';
+import { NavbarPrincipalComponent } from './shared/components/navbar-principal/navbar-principal.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MaterialModule } from './shared/modules/material/material.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarPrincipalComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
